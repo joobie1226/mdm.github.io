5e821bf77eeb4323827c610207f32871dc11b727
joobie1226 <mooredmatthew@hotmail.com>
2025-11-10 18:20:17 -0500

    script.js

--- script.js
+++ script.js
@@ -86,5 +86,29 @@ function selectAnswer(e) {
   nextButton.style.display = "block";
 }
 
+function showScore(){
+  resetState();
+  questionElement.innerHTML = 'You scored ${score} out of $ (questions.length}!';\
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+  currentQuestionIndex++;
+  if(currentQuestionIndex < question.length){
+    showQuestion();
+}else{
+  showScore();
+  }
+}
+
+
+nextButton.addEventListener("click", ()=>{
+  if(currentQuestionIndex < questions.length){
+    handleNextButton();
+  }else{
+    startQuiz();
+})]
+
 startQuiz();
 
